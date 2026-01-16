9c6ee9605fc7468a527ea57946f6d52a6b005698
cstruman <charles.struman@securibox.eu>
2026-01-16 17:58:06 +0000

    added LineTradeAllowanceCharge
    TradeAllowanceCharge can be used in multiple contexts

--- Securibox.FacturX/SpecificationModels/Basic/LineTradeSettlement.cs
+++ Securibox.FacturX/SpecificationModels/Basic/LineTradeSettlement.cs
@@ -7,7 +7,7 @@ namespace Securibox.FacturX.SpecificationModels.Basic
         public BasicWL.TradeTax ApplicableTradeTax { get; set; }
         public BasicWL.SpecifiedPeriod BillingSpecifiedPeriod { get; set; }
         [XmlElement]
-        public BasicWL.TradeAllowanceCharge[] SpecifiedTradeAllowanceCharge { get; set; }
+        public Basic.LineTradeAllowanceCharge[] SpecifiedTradeAllowanceCharge { get; set; }
         public TradeSettlementLineMonetarySummation SpecifiedTradeSettlementLineMonetarySummation { get; set; }
     }
 }
--- Securibox.FacturX/SpecificationModels/BasicWL/TradeAllowanceCharge.cs
+++ Securibox.FacturX/SpecificationModels/BasicWL/TradeAllowanceCharge.cs
@@ -2,15 +2,8 @@
 
 namespace Securibox.FacturX.SpecificationModels.BasicWL
 {
-    public class TradeAllowanceCharge
+    public class TradeAllowanceCharge : Basic.LineTradeAllowanceCharge
     {
-        [XmlElement("ChargeIndicator", Namespace = "urn:un:unece:uncefact:data:standard:ReusableAggregateBusinessInformationEntity:100")]
-        public IndicatorType ChargeIndicator { get; set; }
         public decimal CalculationPercent { get; set; }
-        public Minimum.Amount BasisAmount { get; set; }
-        public Minimum.Amount ActualAmount { get; set; }
-        public string ReasonCode { get; set; }
-        public string Reason { get; set; }
-        public TradeTax CategoryTradeTax { get; set; }
     }
 }
