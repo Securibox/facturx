7f656dde70a96d2c5cb7686bd235d96e95f6738a
Zsombor Hollay-Horvath <hollay.horvath@gmail.com>
2026-04-22 13:55:46 +0200

    FacturXExporter: remove unnecessary dispose of a MemoryStream
    Calling of dispose is unnecessary: https://learn.microsoft.com/en-us/dotnet/api/system.io.memorystream?view=net-8.0#remarks

--- Securibox.FacturX/FacturxExporter.cs
+++ Securibox.FacturX/FacturxExporter.cs
@@ -257,149 +257,138 @@ namespace Securibox.FacturX
                 inputPdfStream = tempStream;
             }
 
-            try
+            using var inputDocument = PdfReader.Open(inputPdfStream, PdfDocumentOpenMode.Import);
+            var outputDocument = new PdfDocument();
+
+            foreach (var page in inputDocument.Pages)
             {
-                using var inputDocument = PdfReader.Open(
-                    inputPdfStream,
-                    PdfDocumentOpenMode.Import
-                );
-                var outputDocument = new PdfDocument();
+                outputDocument.AddPage(page);
+            }
 
-                foreach (var page in inputDocument.Pages)
+            outputDocument.Version = 17;
+
+            string xmlChecksum;
+            byte[] xmlFileBytes;
+            using (var md5 = MD5.Create())
+            {
+                var hashBytes = md5.ComputeHash(xmlStream);
+                xmlChecksum = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+
+                xmlStream.Position = 0;
+                xmlFileBytes = new byte[xmlStream.Length];
+                int bytesRead = 0;
+                while (bytesRead < xmlFileBytes.Length)
                 {
-                    outputDocument.AddPage(page);
-                }
+                    int read = xmlStream.Read(
+                        xmlFileBytes,
+                        bytesRead,
+                        xmlFileBytes.Length - bytesRead
+                    );
 
-                outputDocument.Version = 17;
-
-                string xmlChecksum;
-                byte[] xmlFileBytes;
-                using (var md5 = MD5.Create())
-                {
-                    var hashBytes = md5.ComputeHash(xmlStream);
-                    xmlChecksum = BitConverter
-                        .ToString(hashBytes)
-                        .Replace("-", "")
-                        .ToLowerInvariant();
-
-                    xmlStream.Position = 0;
-                    xmlFileBytes = new byte[xmlStream.Length];
-                    int bytesRead = 0;
-                    while (bytesRead < xmlFileBytes.Length)
+                    if (read == 0)
                     {
-                        int read = xmlStream.Read(
-                            xmlFileBytes,
-                            bytesRead,
-                            xmlFileBytes.Length - bytesRead
-                        );
-                        if (read == 0)
-                            break;
-                        bytesRead += read;
+                        break;
                     }
+
+                    bytesRead += read;
                 }
-
-                PdfDictionary fStreamDict = new PdfDictionary(outputDocument);
-                fStreamDict.CreateStream(xmlFileBytes);
-                fStreamDict.Elements.Remove("/Filter");
-
-                PdfDictionary xmlParamsDict = new PdfDictionary(outputDocument);
-                xmlParamsDict.Elements["/CheckSum"] = new PdfString(xmlChecksum);
-                xmlParamsDict.Elements["/ModDate"] = new PdfString(
-                    "D:" + DateTime.UtcNow.ToString("yyyyMMddHHmmsszzz")
-                );
-                xmlParamsDict.Elements["/Size"] = new PdfInteger(xmlFileBytes.Length);
-
-                fStreamDict.Elements["/Type"] = new PdfName("/EmbeddedFile");
-                fStreamDict.Elements["/Params"] = xmlParamsDict;
-                fStreamDict.Elements["/Subtype"] = new PdfName("/text/xml");
-
-                outputDocument.Internals.AddObject(fStreamDict);
-
-                PdfDictionary af0Dict = new PdfDictionary(outputDocument);
-                af0Dict.Elements["/AFRelationship"] = new PdfName("/Data");
-                af0Dict.Elements["/Desc"] = new PdfString("Factur-X XML file");
-                af0Dict.Elements["/Type"] = new PdfName("/Filespec");
-                af0Dict.Elements["/F"] = new PdfString("factur-x.xml");
-                af0Dict.Elements["/UF"] = new PdfString("factur-x.xml");
-
-                PdfDictionary af1Dict = new PdfDictionary(outputDocument);
-                af1Dict.Elements["/F"] = fStreamDict.Reference;
-                af1Dict.Elements["/UF"] = fStreamDict.Reference;
-
-                af0Dict.Elements["/EF"] = af1Dict;
-                outputDocument.Internals.AddObject(af0Dict);
-
-                var afPdfArray = new PdfArray(outputDocument);
-                afPdfArray.Elements.Add(af0Dict.Reference);
-                outputDocument.Internals.AddObject(afPdfArray);
-                outputDocument.Internals.Catalog.Elements["/AF"] = afPdfArray.Reference;
-
-                var dateTimeNow = DateTime.UtcNow;
-                var xmpmeta = Resources
-                    .PdfMetadataTemplate.Replace(
-                        "{{CreationDate}}",
-                        dateTimeNow.ToString("yyyy-MM-ddTHH:mm:sszzz")
-                    )
-                    .Replace("{{ModificationDate}}", dateTimeNow.ToString("yyyy-MM-ddTHH:mm:sszzz"))
-                    .Replace("{{DocumentTitle}}", documentTitle)
-                    .Replace("{{DocumentDescription}}", documentDescription)
-                    .Replace("{{ConformanceLevel}}", conformanceLevel.Name.ToUpperInvariant());
-
-                xmpmeta += new string(' ', 2000);
-                var metadataBytes = new UTF8Encoding(false).GetBytes(xmpmeta);
-
-                PdfDictionary metadataDictionary = new PdfDictionary(outputDocument);
-                metadataDictionary.CreateStream(metadataBytes);
-                metadataDictionary.Elements["/Type"] = new PdfName("/Metadata");
-                metadataDictionary.Elements["/Subtype"] = new PdfName("/XML");
-                metadataDictionary.Elements.Remove("/Filter");
-
-                outputDocument.Internals.AddObject(metadataDictionary);
-                outputDocument.Internals.Catalog.Elements["/Metadata"] =
-                    metadataDictionary.Reference;
-
-                var namesPdfArray = new PdfArray(outputDocument);
-                namesPdfArray.Elements.Add(new PdfString("factur-x.xml"));
-                namesPdfArray.Elements.Add(af0Dict.Reference);
-
-                PdfDictionary embeddedFilesDict = new PdfDictionary(outputDocument);
-                embeddedFilesDict.Elements["/Names"] = namesPdfArray;
-
-                PdfDictionary namesDict = new PdfDictionary(outputDocument);
-                namesDict.Elements["/EmbeddedFiles"] = embeddedFilesDict;
-                outputDocument.Internals.Catalog.Elements["/Names"] = namesDict;
-
-                PdfDictionary rgbProfileDict = new PdfDictionary(outputDocument);
-                rgbProfileDict.CreateStream(Resources.sRGB_IEC61966_2_1);
-                rgbProfileDict.Elements["/N"] = new PdfInteger(3);
-                rgbProfileDict.Elements.Remove("/Filter");
-
-                outputDocument.Internals.AddObject(rgbProfileDict);
-
-                PdfDictionary outputIntent0Dict = new PdfDictionary(outputDocument);
-                outputIntent0Dict.Elements["/DestOutputProfile"] = rgbProfileDict.Reference;
-                outputIntent0Dict.Elements["/OutputConditionIdentifier"] = new PdfString(
-                    "sRGB IEC61966-2.1"
-                );
-                outputIntent0Dict.Elements["/S"] = new PdfName("/GTS_PDFA1");
-                outputIntent0Dict.Elements["/Type"] = new PdfName("/OutputIntent");
-                outputDocument.Internals.AddObject(outputIntent0Dict);
-
-                var outputIntentsArray = new PdfArray(outputDocument);
-                outputIntentsArray.Elements.Add(outputIntent0Dict.Reference);
-                outputDocument.Internals.Catalog.Elements["/OutputIntents"] = outputIntentsArray;
-                outputDocument.Info.Creator = "Securibox";
-                MemoryStream memoryStream = new MemoryStream();
-                outputDocument.Save(memoryStream);
-                memoryStream.Seek(0, SeekOrigin.Begin);
-
-                _logger.LogInformation($"Successfully generated the Factur-X PDF stream.");
-                return memoryStream;
             }
-            finally
-            {
-                tempStream?.Dispose();
-            }
+
+            PdfDictionary fStreamDict = new PdfDictionary(outputDocument);
+            fStreamDict.CreateStream(xmlFileBytes);
+            fStreamDict.Elements.Remove("/Filter");
+
+            PdfDictionary xmlParamsDict = new PdfDictionary(outputDocument);
+            xmlParamsDict.Elements["/CheckSum"] = new PdfString(xmlChecksum);
+            xmlParamsDict.Elements["/ModDate"] = new PdfString(
+                "D:" + DateTime.UtcNow.ToString("yyyyMMddHHmmsszzz")
+            );
+            xmlParamsDict.Elements["/Size"] = new PdfInteger(xmlFileBytes.Length);
+
+            fStreamDict.Elements["/Type"] = new PdfName("/EmbeddedFile");
+            fStreamDict.Elements["/Params"] = xmlParamsDict;
+            fStreamDict.Elements["/Subtype"] = new PdfName("/text/xml");
+
+            outputDocument.Internals.AddObject(fStreamDict);
+
+            PdfDictionary af0Dict = new PdfDictionary(outputDocument);
+            af0Dict.Elements["/AFRelationship"] = new PdfName("/Data");
+            af0Dict.Elements["/Desc"] = new PdfString("Factur-X XML file");
+            af0Dict.Elements["/Type"] = new PdfName("/Filespec");
+            af0Dict.Elements["/F"] = new PdfString("factur-x.xml");
+            af0Dict.Elements["/UF"] = new PdfString("factur-x.xml");
+
+            PdfDictionary af1Dict = new PdfDictionary(outputDocument);
+            af1Dict.Elements["/F"] = fStreamDict.Reference;
+            af1Dict.Elements["/UF"] = fStreamDict.Reference;
+
+            af0Dict.Elements["/EF"] = af1Dict;
+            outputDocument.Internals.AddObject(af0Dict);
+
+            var afPdfArray = new PdfArray(outputDocument);
+            afPdfArray.Elements.Add(af0Dict.Reference);
+            outputDocument.Internals.AddObject(afPdfArray);
+            outputDocument.Internals.Catalog.Elements["/AF"] = afPdfArray.Reference;
+
+            var dateTimeNow = DateTime.UtcNow;
+            var xmpmeta = Resources
+                .PdfMetadataTemplate.Replace(
+                    "{{CreationDate}}",
+                    dateTimeNow.ToString("yyyy-MM-ddTHH:mm:sszzz")
+                )
+                .Replace("{{ModificationDate}}", dateTimeNow.ToString("yyyy-MM-ddTHH:mm:sszzz"))
+                .Replace("{{DocumentTitle}}", documentTitle)
+                .Replace("{{DocumentDescription}}", documentDescription)
+                .Replace("{{ConformanceLevel}}", conformanceLevel.Name.ToUpperInvariant());
+
+            var metadataBytes = new UTF8Encoding(false).GetBytes(xmpmeta);
+
+            PdfDictionary metadataDictionary = new PdfDictionary(outputDocument);
+            metadataDictionary.CreateStream(metadataBytes);
+            metadataDictionary.Elements["/Type"] = new PdfName("/Metadata");
+            metadataDictionary.Elements["/Subtype"] = new PdfName("/XML");
+            metadataDictionary.Elements.Remove("/Filter");
+
+            outputDocument.Internals.AddObject(metadataDictionary);
+            outputDocument.Internals.Catalog.Elements["/Metadata"] = metadataDictionary.Reference;
+
+            var namesPdfArray = new PdfArray(outputDocument);
+            namesPdfArray.Elements.Add(new PdfString("factur-x.xml"));
+            namesPdfArray.Elements.Add(af0Dict.Reference);
+
+            PdfDictionary embeddedFilesDict = new PdfDictionary(outputDocument);
+            embeddedFilesDict.Elements["/Names"] = namesPdfArray;
+
+            PdfDictionary namesDict = new PdfDictionary(outputDocument);
+            namesDict.Elements["/EmbeddedFiles"] = embeddedFilesDict;
+            outputDocument.Internals.Catalog.Elements["/Names"] = namesDict;
+
+            PdfDictionary rgbProfileDict = new PdfDictionary(outputDocument);
+            rgbProfileDict.CreateStream(Resources.sRGB_IEC61966_2_1);
+            rgbProfileDict.Elements["/N"] = new PdfInteger(3);
+            rgbProfileDict.Elements.Remove("/Filter");
+
+            outputDocument.Internals.AddObject(rgbProfileDict);
+
+            PdfDictionary outputIntent0Dict = new PdfDictionary(outputDocument);
+            outputIntent0Dict.Elements["/DestOutputProfile"] = rgbProfileDict.Reference;
+            outputIntent0Dict.Elements["/OutputConditionIdentifier"] = new PdfString(
+                "sRGB IEC61966-2.1"
+            );
+            outputIntent0Dict.Elements["/S"] = new PdfName("/GTS_PDFA1");
+            outputIntent0Dict.Elements["/Type"] = new PdfName("/OutputIntent");
+            outputDocument.Internals.AddObject(outputIntent0Dict);
+
+            var outputIntentsArray = new PdfArray(outputDocument);
+            outputIntentsArray.Elements.Add(outputIntent0Dict.Reference);
+            outputDocument.Internals.Catalog.Elements["/OutputIntents"] = outputIntentsArray;
+            outputDocument.Info.Creator = "Securibox";
+            MemoryStream memoryStream = new MemoryStream();
+            outputDocument.Save(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            _logger.LogInformation($"Successfully generated the Factur-X PDF stream.");
+            return memoryStream;
         }
 
         private ILogger<FacturxExporter> InitializeLogger(ILogger<FacturxExporter>? logger)
